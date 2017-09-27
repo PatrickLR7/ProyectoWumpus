628c1b93dd574fca0156b207d0e398cececb81b7
eduardobs17 <eduardobs17@outlook.com>
2017-09-27 10:44:21 -0600

    Se modificó Grafo

--- app/src/main/java/com/example/carlos/wumpusproject/Grafo.java
+++ app/src/main/java/com/example/carlos/wumpusproject/Grafo.java
@@ -12,8 +12,9 @@ public class Grafo {
     private boolean[][] matriz;
     /** Tamaño del juego. */
     private int totalCuevas;
+
     /** Lista de todas las cuevas del juego. Cada cueva está representada por un numero. Cada elemento de la lista será el tipo de cueva. */
-    private List<Integer> listaCuevas;
+    /*private List<Integer> listaCuevas;*/
 
     public Grafo(int tam) {
         totalCuevas = tam;
@@ -23,11 +24,11 @@ public class Grafo {
                 matriz[x][y] = false;
             }
         }
-        listaCuevas = new LinkedList<>();
+        //listaCuevas = new LinkedList<>();
     }
 
     /**
-     * Se agrega un camino entre las cuevas especificadas.
+     * Se agrega un camino entre los nodos especificados.
      */
     public void addArista(int nodoX, int nodoY) {
         if (matriz[nodoX][nodoY] == false) {
@@ -36,7 +37,7 @@ public class Grafo {
     }
 
     /**
-     * Se borra un camino entre las cuevas especificadas.
+     * Se borra un camino entre loa nodos especificados.
      */
     public void deleteArista(int nodoX, int nodoY) {
         if (matriz[nodoX][nodoY] == true) {
@@ -44,7 +45,9 @@ public class Grafo {
         }
     }
 
-    //Se obtienen las cuevas adyacentes a la cueva especificada por parametro.
+    /**
+     * Se obtienen los nodos adyacentes al nodo especificado por parametro.
+     */
     public List<Integer> obtenerVecinos(int Nodo) {
         List<Integer> lista = new LinkedList<>();
         for (int x = 0; x < totalCuevas; x++) {
@@ -63,7 +66,7 @@ public class Grafo {
      * 3 => Pozo
      * 4 => Wumpus
      */
-    public void asignarTipo() {
+    /*public void asignarTipo() {
         boolean wumpus = false;
         for (int x = 0; x < totalCuevas; x++) {
             int tipo = (int) (Math.random() * 4);
@@ -76,5 +79,5 @@ public class Grafo {
                 }
             }
         }
-    }
+    }*/
 }
