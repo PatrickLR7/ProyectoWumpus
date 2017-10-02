6b9558a8a89efac7aac587aac7830472a533d324
eduardobs17 <eduardobs17@outlook.com>
2017-10-02 11:48:36 -0600

    funcionalidad botones iniciales

--- app/src/main/java/com/example/carlos/wumpusproject/MainActivity.java
+++ app/src/main/java/com/example/carlos/wumpusproject/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.carlos.wumpusproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button poliedro;
     Button laberinto;
@@ -14,10 +15,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_mode);
+        setContentView(R.layout.activity_main);
 
         poliedro = (Button) findViewById(R.id.PoliRegButton);
         poliedro.setOnClickListener((View.OnClickListener) this);
+        laberinto = (Button) findViewById(R.id.LabIrregButton);
+        laberinto.setOnClickListener((View.OnClickListener) this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.PoliRegButton:
+                Intent i1 = new Intent(getApplicationContext(), GrafosRegulares.class);
+                startActivity(i1);
+                break;
+            case R.id.LabIrregButton:
+                break;
+        }
     }
 }
