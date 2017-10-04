6268fc8a4e7420fca9023462d788e6cd4b03b221
Eduardo BS <eduardobs17@outlook.com>
2017-10-03 21:55:36 -0600

    Funcion onClick botones del graphdraw implementada

--- app/src/main/java/com/example/carlos/wumpusproject/GraphDrawActivity.java
+++ app/src/main/java/com/example/carlos/wumpusproject/GraphDrawActivity.java
@@ -1,59 +1,44 @@
 package com.example.carlos.wumpusproject;
 
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-
-import java.util.ArrayList;
-import java.util.Iterator;
+import android.widget.Toast;
 
 public class GraphDrawActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ArrayList<ImageButton> listaBotones = new ArrayList<ImageButton>();
-    private String nombreBoton = "";
+    private ImageButton[][] matriz;
     private Button finalizeButton;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph_draw);
 
-        finalizeButton = (Button) findViewById(R.id.graphDrawButton);
-        finalizeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Saving graph to database
-
-            }
-        });
-    }
-
-    public void inicializarBotones(View v) {
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
-                nombreBoton = "imageButton";
-                nombreBoton += i;
-                nombreBoton += j;
-                int btnId = getResources().getIdentifier(nombreBoton, "id", getPackageName());
-                listaBotones.add((ImageButton) findViewById(btnId));
+        matriz = new ImageButton[10][10];
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
+                String nombreBoton = "imageButton" + x + y;
+                int id = getResources().getIdentifier(nombreBoton, "id", getPackageName());
+                matriz[x][y] = (ImageButton) findViewById(id);
+                matriz[x][y].setOnClickListener(this);
             }
         }
+
+        finalizeButton = (Button) findViewById(R.id.graphDrawButton);
+        finalizeButton.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View view) {
-        int btnId = getResources().getIdentifier(nombreBoton, "id", getPackageName());
-
-        for (int i = 0; i < listaBotones.size(); i++){
-           if(listaBotones.get(i).getId() == view.getId()) {
-              //  listaBotones.get(i).setBackground(R.mipmap.cueva);
-           }
+    public void onClick(View v) {
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
+                if (matriz[x][y].getId() == v.getId()) {
+                    Toast.makeText(this, "imagenbutton" + x + y, Toast.LENGTH_LONG).show();
+                }
+            }
         }
     }
 }
-
