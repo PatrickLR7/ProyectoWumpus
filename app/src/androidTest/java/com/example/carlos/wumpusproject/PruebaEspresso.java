b4d928bc139427c350d70be01bda1e749c971530
Karman <krmanxv@gmail.com>
2017-10-12 23:58:45 -0600

    Primeras dos pruebas de Espresso

---
+++ app/src/androidTest/java/com/example/carlos/wumpusproject/PruebaEspresso.java
@@ -0,0 +1,69 @@
+package com.example.carlos.wumpusproject;
+
+import android.os.SystemClock;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.action.ViewActions;
+import android.test.ActivityInstrumentationTestCase2;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+
+public class PruebaEspresso extends ActivityInstrumentationTestCase2<GameModeActivity>
+{
+
+    public PruebaEspresso()
+    {
+        super(GameModeActivity.class);
+    }
+
+
+    @Before
+    public void setUp() throws Exception
+    {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+    }
+
+
+    @Test
+    public void pruebaPrimerasDosPantallas()
+    {
+        getActivity();
+        onView(withId(R.id.individualMode)) //Entra al modo individual
+            .perform(click());
+        SystemClock.sleep(1000);
+        onView(isRoot()).perform(ViewActions.pressBack()); //Retorna
+        SystemClock.sleep(1000);
+        onView(withId(R.id.socialMode)) //Ahora entra al modo social
+                .perform(click());
+        SystemClock.sleep(1000);
+
+        onView(withId(R.id.PoliRegButton)) //Ahora entra al modo social
+                .perform(click());
+        SystemClock.sleep(1000);
+        onView(isRoot()).perform(ViewActions.pressBack()); //Retorna
+        SystemClock.sleep(1000);
+        onView(withId(R.id.LabIrregButton)) //Ahora entra al modo social
+                .perform(click());
+    }
+
+/*
+    @After
+    public void tearDown() throws Exception
+    {
+        super.tearDown();
+    }
+*/
+}
