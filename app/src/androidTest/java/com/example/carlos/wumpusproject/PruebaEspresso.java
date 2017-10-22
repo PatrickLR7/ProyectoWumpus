6920a5c8c056ae890c8655c798aefcf5b67513d0
Karman <krmanxv@gmail.com>
2017-10-22 13:00:39 -0600

    Pruebas Espresso de primera iteración terminadas

--- app/src/androidTest/java/com/example/carlos/wumpusproject/PruebaEspresso.java
+++ app/src/androidTest/java/com/example/carlos/wumpusproject/PruebaEspresso.java
@@ -2,18 +2,40 @@ package com.example.carlos.wumpusproject;
 
 import android.os.SystemClock;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.example.carlos.wumpusproject.Pruebas.childAtPosition;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(AndroidJUnit4.class)
 
@@ -34,9 +56,10 @@ public class PruebaEspresso extends ActivityInstrumentationTestCase2<MainActivit
     }
 
     @Test
-    public void pruebaPrimerasDosPantallas()
+    public void probar()
     {
         getActivity();
+        /*
         SystemClock.sleep(1000);
         onView(withId(R.id.PoliRegButton)) //Entra a ver poligonos regulares
                 .perform(click());
@@ -56,18 +79,20 @@ public class PruebaEspresso extends ActivityInstrumentationTestCase2<MainActivit
         onView(withId(R.id.Dodecaedro)) //Click en la imagen del dodecahedro
                 .perform(click());
         SystemClock.sleep(4000);
-
         onView(isRoot()).perform(ViewActions.pressBack()); //Retorna
+        */
         SystemClock.sleep(2000);
         onView(withId(R.id.LabIrregButton)) //Ahora entra a dibujar laberinto
                 .perform(click());
         SystemClock.sleep(1000);
         onView(withId(R.id.imageButton10)) //Arista que va desde esta cueva
                 .perform(click());
-        SystemClock.sleep(1000);
+        SystemClock.sleep(500);
         onView(withId(R.id.imageButton38)) //hasta esta otra
                 .perform(click());
-        SystemClock.sleep(1000);
+        SystemClock.sleep(500);
+
+        /* Pruebas de validación
         onView(withId(R.id.imageButton32)) //Intenta hacer una arista
                 .perform(click());
         SystemClock.sleep(1000);
@@ -86,46 +111,125 @@ public class PruebaEspresso extends ActivityInstrumentationTestCase2<MainActivit
         onView(withId(R.id.imageButton1110)) //paralela
                 .perform(click());
         SystemClock.sleep(1000);
+        */
 
         //hace un laberinto con sentido
         onView(withId(R.id.imageButton10)) //Arista que va desde esta cueva
                 .perform(click());
-        SystemClock.sleep(1000);
+        SystemClock.sleep(500);
         onView(withId(R.id.imageButton72)) //hasta esta otra
                 .perform(click());
-        SystemClock.sleep(1000);
+        SystemClock.sleep(500);
         onView(withId(R.id.imageButton72)) //Arista que va desde esta cueva
                 .perform(click());
-        SystemClock.sleep(1000);
+        SystemClock.sleep(500);
         onView(withId(R.id.imageButton710)) //hasta esta otra
                 .perform(click());
-        SystemClock.sleep(1000);
+        SystemClock.sleep(500);
         onView(withId(R.id.imageButton72)) //Arista que va desde esta cueva
                 .perform(click());
-        SystemClock.sleep(1000);
-        onView(withId(R.id.imageButton52)) //hasta esta otra
+        SystemClock.sleep(500);
+        onView(withId(R.id.imageButton34)) //hasta esta otra
                 .perform(click());
-        SystemClock.sleep(1000);
-        onView(withId(R.id.imageButton52)) //Arista que va desde esta cueva
+        SystemClock.sleep(500);
+        onView(withId(R.id.imageButton34)) //Arista que va desde esta cueva
                 .perform(click());
-        SystemClock.sleep(1000);
-        onView(withId(R.id.imageButton36)) //hasta esta otra
-                .perform(click());
-        SystemClock.sleep(1000);
-        onView(withId(R.id.imageButton36)) //Arista que va desde esta cueva
-                .perform(click());
-        SystemClock.sleep(1000);
+        SystemClock.sleep(500);
         onView(withId(R.id.imageButton38)) //hasta esta otra
                 .perform(click());
-        SystemClock.sleep(1000);
+        SystemClock.sleep(500);
         onView(withId(R.id.imageButton38)) //Arista que va desde esta cueva
                 .perform(click());
-        SystemClock.sleep(1000);
+        SystemClock.sleep(500);
         onView(withId(R.id.imageButton710)) //hasta esta otra
                 .perform(click());
-        SystemClock.sleep(1000);
+        SystemClock.sleep(500);
+        onView(withId(R.id.imageButton72)) //Arista que va desde esta cueva
+                .perform(click());
+        SystemClock.sleep(500);
+        onView(withId(R.id.imageButton116)) //hasta esta otra
+                .perform(click());
+        SystemClock.sleep(500);
+        onView(withId(R.id.imageButton710)) //Arista que va desde esta cueva
+                .perform(click());
+        SystemClock.sleep(500);
+        onView(withId(R.id.imageButton116)) //hasta esta otra
+                .perform(click());
+
+        SystemClock.sleep(500);
+        onView(withId(R.id.imageButton56)) //Arista que va desde esta cueva
+                .perform(click());
+        SystemClock.sleep(500);
+        onView(withId(R.id.imageButton116)) //hasta esta otra
+                .perform(click());
+        SystemClock.sleep(500);
+        onView(withId(R.id.imageButton56)) //Arista que va desde esta cueva
+                .perform(click());
+        SystemClock.sleep(500);
+        onView(withId(R.id.imageButton710)) //hasta esta otra
+                .perform(click());
+        SystemClock.sleep(500);
+        onView(withId(R.id.imageButton56)) //Arista que va desde esta cueva
+                .perform(click());
+        SystemClock.sleep(500);
+        onView(withId(R.id.imageButton72)) //hasta esta otra
+                .perform(click());
+        SystemClock.sleep(2000);
         //Termina de hacer un laberinto con sentido
 
+        postLab();
+    }
 
+    public void postLab()
+    {
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.finalizeDrawButton), withText("Guardar Grafo"), isDisplayed()));
+        appCompatButton2.perform(click());
+        SystemClock.sleep(500);
+
+        ViewInteraction editText = onView(
+                allOf(withClassName(is("android.widget.EditText")),
+                        withParent(allOf(withId(R.id.custom),
+                                withParent(withId(R.id.customPanel)))),
+                        isDisplayed()));
+        editText.perform(replaceText("prueba-a"), closeSoftKeyboard());
+        SystemClock.sleep(500);
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(android.R.id.button1), withText("Guardar")));
+        appCompatButton3.perform(scrollTo(), click());
+        SystemClock.sleep(500);
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(android.R.id.button1), withText("OK")));
+        appCompatButton4.perform(scrollTo(), click());
+        SystemClock.sleep(500);
+
+        ViewInteraction appCompatButton5 = onView(
+                allOf(withId(R.id.elegirBiblio), withText("Elegir de la Biblioteca"), isDisplayed()));
+        appCompatButton5.perform(click());
+        SystemClock.sleep(500);
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(android.R.id.text1), withText("prueba-a"),
+                        childAtPosition(
+                                allOf(withId(R.id.select_dialog_listview),
+                                        withParent(withId(R.id.contentPanel))),
+                                0),
+                        isDisplayed()));
+        appCompatTextView.perform(click());
+        SystemClock.sleep(500);
+
+        ViewInteraction appCompatButton6 = onView(
+                allOf(withId(android.R.id.button1), withText("Aceptar")));
+        appCompatButton6.perform(scrollTo(), click());
+
+        /* Bluetooth no está listo
+        ViewInteraction appCompatButton7 = onView(
+                allOf(withId(R.id.compartirBluetooth), withText("Compartir por Bluetooth"), isDisplayed()));
+        appCompatButton7.perform(click());
+        SystemClock.sleep(500);
+        onView(isRoot()).perform(ViewActions.pressBack()); //Retorna
+        */
+        SystemClock.sleep(1000);
     }
 }
---
+++ app/src/androidTest/java/com/example/carlos/wumpusproject/Pruebas.java
@@ -0,0 +1,192 @@
+package com.example.carlos.wumpusproject;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(AndroidJUnit4.class)
+public class Pruebas {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void pruebas() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.LabIrregButton), withText("Laberinto Irregular"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.imageButton10), isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withId(R.id.imageButton38), isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+        ViewInteraction appCompatImageButton3 = onView(
+                allOf(withId(R.id.imageButton10), isDisplayed()));
+        appCompatImageButton3.perform(click());
+
+        ViewInteraction appCompatImageButton4 = onView(
+                allOf(withId(R.id.imageButton72), isDisplayed()));
+        appCompatImageButton4.perform(click());
+
+        ViewInteraction appCompatImageButton5 = onView(
+                allOf(withId(R.id.imageButton72), isDisplayed()));
+        appCompatImageButton5.perform(click());
+
+        ViewInteraction appCompatImageButton6 = onView(
+                allOf(withId(R.id.imageButton34), isDisplayed()));
+        appCompatImageButton6.perform(click());
+
+        ViewInteraction appCompatImageButton7 = onView(
+                allOf(withId(R.id.imageButton34), isDisplayed()));
+        appCompatImageButton7.perform(click());
+
+        ViewInteraction appCompatImageButton8 = onView(
+                allOf(withId(R.id.imageButton38), isDisplayed()));
+        appCompatImageButton8.perform(click());
+
+        ViewInteraction appCompatImageButton9 = onView(
+                allOf(withId(R.id.imageButton38), isDisplayed()));
+        appCompatImageButton9.perform(click());
+
+        ViewInteraction appCompatImageButton10 = onView(
+                allOf(withId(R.id.imageButton710), isDisplayed()));
+        appCompatImageButton10.perform(click());
+
+        ViewInteraction appCompatImageButton11 = onView(
+                allOf(withId(R.id.imageButton72), isDisplayed()));
+        appCompatImageButton11.perform(click());
+
+        ViewInteraction appCompatImageButton12 = onView(
+                allOf(withId(R.id.imageButton710), isDisplayed()));
+        appCompatImageButton12.perform(click());
+
+        ViewInteraction appCompatImageButton13 = onView(
+                allOf(withId(R.id.imageButton116), isDisplayed()));
+        appCompatImageButton13.perform(click());
+
+        ViewInteraction appCompatImageButton14 = onView(
+                allOf(withId(R.id.imageButton710), isDisplayed()));
+        appCompatImageButton14.perform(click());
+
+        ViewInteraction appCompatImageButton15 = onView(
+                allOf(withId(R.id.imageButton116), isDisplayed()));
+        appCompatImageButton15.perform(click());
+
+        ViewInteraction appCompatImageButton16 = onView(
+                allOf(withId(R.id.imageButton72), isDisplayed()));
+        appCompatImageButton16.perform(click());
+
+        ViewInteraction appCompatImageButton17 = onView(
+                allOf(withId(R.id.imageButton116), isDisplayed()));
+        appCompatImageButton17.perform(click());
+
+        ViewInteraction appCompatImageButton18 = onView(
+                allOf(withId(R.id.imageButton56), isDisplayed()));
+        appCompatImageButton18.perform(click());
+
+        ViewInteraction appCompatImageButton19 = onView(
+                allOf(withId(R.id.imageButton56), isDisplayed()));
+        appCompatImageButton19.perform(click());
+
+        ViewInteraction appCompatImageButton20 = onView(
+                allOf(withId(R.id.imageButton72), isDisplayed()));
+        appCompatImageButton20.perform(click());
+
+        ViewInteraction appCompatImageButton21 = onView(
+                allOf(withId(R.id.imageButton56), isDisplayed()));
+        appCompatImageButton21.perform(click());
+
+        ViewInteraction appCompatImageButton22 = onView(
+                allOf(withId(R.id.imageButton710), isDisplayed()));
+        appCompatImageButton22.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.finalizeDrawButton), withText("Guardar Grafo"), isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction editText = onView(
+                allOf(withClassName(is("android.widget.EditText")),
+                        withParent(allOf(withId(R.id.custom),
+                                withParent(withId(R.id.customPanel)))),
+                        isDisplayed()));
+        editText.perform(replaceText("prueba-a"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(android.R.id.button1), withText("Guardar")));
+        appCompatButton3.perform(scrollTo(), click());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(android.R.id.button1), withText("OK")));
+        appCompatButton4.perform(scrollTo(), click());
+
+        ViewInteraction appCompatButton5 = onView(
+                allOf(withId(R.id.elegirBiblio), withText("Elegir de la Biblioteca"), isDisplayed()));
+        appCompatButton5.perform(click());
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(android.R.id.text1), withText("prueba-a"),
+                        childAtPosition(
+                                allOf(withId(R.id.select_dialog_listview),
+                                        withParent(withId(R.id.contentPanel))),
+                                0),
+                        isDisplayed()));
+        appCompatTextView.perform(click());
+
+        ViewInteraction appCompatButton6 = onView(
+                allOf(withId(android.R.id.button1), withText("Aceptar")));
+        appCompatButton6.perform(scrollTo(), click());
+
+        ViewInteraction appCompatButton7 = onView(
+                allOf(withId(R.id.compartirBluetooth), withText("Compartir por Bluetooth"), isDisplayed()));
+        appCompatButton7.perform(click());
+
+    }
+
+    public static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
