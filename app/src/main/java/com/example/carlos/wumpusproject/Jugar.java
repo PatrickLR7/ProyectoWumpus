2c9348e33cc24a31718c375ea9f1fa6b6280f813
KarmanXV <krmanxv@gmail.com>
2017-10-25 10:59:19 -0600

    Clase para jugar

--- app/src/main/java/com/example/carlos/wumpusproject/Jugar.java
+++ app/src/main/java/com/example/carlos/wumpusproject/Jugar.java
@@ -1,5 +1,6 @@
 package com.example.carlos.wumpusproject;
 
+import com.example.carlos.wumpusproject.utils.Config;
 import com.example.carlos.wumpusproject.utils.Grafo;
 
 import java.util.List;
@@ -7,22 +8,38 @@ import java.util.List;
 public class Jugar
 {
     private Grafo tablero;
+    private List<Integer> tiposCueva;
     private int flechasRestantes = 5;
-    private int cuevaActual = -1;
+    private int cuevaActual = -1; //Posición actual del jugador
 
-    public Jugar(Grafo lab)
+    public Jugar() //Recibe el tablero con el que se va a jugar
     {
-        tablero = lab;
+        tablero = Config.laberinto;
+        tiposCueva = Config.tiposDeCuevas;
     }
 
     public void mostrarIndicios()
     {
         List<Integer> vecinos = tablero.obtenerVecinos(cuevaActual);
+
         int vecinoActual = -1;
+        int tipo = -1;
         for(int i = 0; i < vecinos.size(); i++)
         {
             vecinoActual = vecinos.get(i);
-
+            tipo = tiposCueva.get(vecinoActual);
+            switch (tipo)
+            {
+                case 1:
+                    //Wumpus
+                    break;
+                case 2:
+                    //Pozo
+                    break;
+                case 3:
+                    //Murc
+                    break;
+            }
         }
     }
 }
