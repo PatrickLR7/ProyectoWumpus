b277e1538725c88ef1d0b6fcd2e6fce55049addc
KarmanXV <krmanxv@gmail.com>
2017-11-01 09:53:09 -0600

    Indicios

--- app/src/main/java/com/example/carlos/wumpusproject/Jugar.java
+++ app/src/main/java/com/example/carlos/wumpusproject/Jugar.java
@@ -1,13 +1,18 @@
 package com.example.carlos.wumpusproject;
 
 import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.widget.Toast;
 import android.os.Vibrator;
+import android.os.VibrationEffect;
 
 import com.example.carlos.wumpusproject.utils.Config;
 import com.example.carlos.wumpusproject.utils.Grafo;
 
 import java.util.List;
+
+import static android.content.Context.VIBRATOR_SERVICE;
 
 public class Jugar
 {
@@ -15,11 +20,17 @@ public class Jugar
     private List<Integer> tiposCueva;
     private int flechasRestantes = 5;
     private int cuevaActual = -1; //Posición actual del jugador
+    private Context context;
+    private Vibrator v;
+    private MediaPlayer mp;
 
-    public Jugar() //Recibe el tablero con el que se va a jugar
+    public Jugar(Context con) //Recibe el tablero con el que se va a jugar
     {
+        context = con;
         tablero = Config.laberinto;
         tiposCueva = Config.tiposDeCuevas;
+        v = (Vibrator)context.getSystemService(VIBRATOR_SERVICE); //Vibrador que se usa después
+        mp = MediaPlayer.create(context, R.raw.batsound); //Inicializa el efecto de sonido
     }
 
     public void mostrarIndicios()
@@ -36,12 +47,29 @@ public class Jugar
             {
                 case 1:
                     //Mensaje diciendo que hay wumpus
+                    Toast.makeText(context, "Hay un desagradable olor a wumpus", Toast.LENGTH_LONG).show();
                     break;
                 case 2:
                     //Pozo vibra
+                    if(v.hasVibrator()) //Verifica que el dispositivo tiene la capacidad de vibrar
+                    {
+                        if (Build.VERSION.SDK_INT >= 26) //La vibración se maneja distinto según el SDK
+                        {
+                            v.vibrate(VibrationEffect.createOneShot(150, VibrationEffect.DEFAULT_AMPLITUDE));
+                        }
+                        else
+                        {
+                            v.vibrate(150);
+                        }
+                    }
+                    else //Si no puede vibrar, muestra un mensaje
+                    {
+                        Toast.makeText(context, "Se siente una brisa fría", Toast.LENGTH_LONG).show();
+                    }
                     break;
                 case 3:
-                    //Murcielagos con efecto de sonido
+                    //Murciélagos con efecto de sonido
+                    mp.start(); //Suena el efecto de sonido de murciélagos
                     break;
             }
         }
