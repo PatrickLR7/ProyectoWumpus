0538663064c286c50ba1066d117746148d14c201
b40999 <eduardobs17@outlook.com>
2017-11-15 10:11:25 -0600

    SimpleCamera, Geofence transitions y config actualizados en iteracion3

--- app/src/main/java/com/example/carlos/wumpusproject/beyondAR/SimpleCamera.java
+++ app/src/main/java/com/example/carlos/wumpusproject/beyondAR/SimpleCamera.java
@@ -1,9 +1,12 @@
 package com.example.carlos.wumpusproject.beyondAR;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -19,9 +22,17 @@ import com.beyondar.android.world.BeyondarObject;
 import com.beyondar.android.world.World;
 import com.example.carlos.wumpusproject.R;
 import com.example.carlos.wumpusproject.utils.Config;
+import com.example.carlos.wumpusproject.utils.GeofenceTransitionIntentService;
 import com.example.carlos.wumpusproject.utils.Jugar;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 
 public class SimpleCamera extends AppCompatActivity implements OnClickBeyondarObjectListener{
@@ -31,11 +42,18 @@ public class SimpleCamera extends AppCompatActivity implements OnClickBeyondarOb
     private CustomWorldHelper customWorldHelper;
     private Vector<Double> coordenadasIniciales;
     private Jugar jugar;
+    private GeofencingClient mGeofencingClient;
+    private List<Geofence> mGeofenceList;
+    private PendingIntent mGeofencePendingIntent;
+
+    private Boolean location;
+    private Boolean camera;
 
     /** Llamado cuando se crea la actividad. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Config.camera = this;
 
         String Permiso[] = {"android.permission.CAMERA", "android.permission.ACCESS_FINE_LOCATION"};
         // Start home activity
@@ -45,6 +63,7 @@ public class SimpleCamera extends AppCompatActivity implements OnClickBeyondarOb
 
         setContentView(R.layout.activity_simple_camera);
         customWorldHelper = new CustomWorldHelper();
+        jugar = new Jugar(this);
         coordenadasIniciales = Config.coordenadasIniciales;
 
         mBeyondarFragment = (BeyondarFragmentSupport) getSupportFragmentManager().findFragmentById(
@@ -74,9 +93,77 @@ public class SimpleCamera extends AppCompatActivity implements OnClickBeyondarOb
         // We also can see the Frames per seconds
         mBeyondarFragment.showFPS(true);
 
-        jugar = new Jugar(this);
-        jugar.mostrarIndicios();
+        mGeofencingClient = LocationServices.getGeofencingClient(this);
+        mGeofenceList = new ArrayList<>();
+
+        List< Vector<Double> > coordenadasCuevas = Config.coordenadasCuevas;
+        for (int i = 0; i < coordenadasCuevas.size(); ++i) {
+            Vector<Double> coord = coordenadasCuevas.get(i);
+            if (!(coord.get(0) == 0 && coord.get(1) == 0)) {
+                mGeofenceList.add(new Geofence.Builder()
+                        // Set the request ID of the geofence. This is a string to identify this geofence.
+                        .setRequestId(i + "")
+
+                        .setCircularRegion( coord.get(0),
+                                coord.get(1),
+                                Config.radioCuevas
+                        )
+                        .setExpirationDuration(Config.tiempoExpiracion)
+                        .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                                Geofence.GEOFENCE_TRANSITION_EXIT)
+                        .build());
+            }
+        }
+
+        this.addGeofences();
+
+        // jugar = new Jugar(this);
+        // jugar.mostrarIndicios();
     }
+
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofences(mGeofenceList);
+        return builder.build();
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+        if (mGeofencePendingIntent != null) {
+            return mGeofencePendingIntent;
+        }
+        Intent intent = new Intent(this, GeofenceTransitionIntentService.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+        mGeofencePendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
+        return mGeofencePendingIntent;
+    }
+
+    public void addGeofences(){
+        final SimpleCamera camera = this;
+        try {
+            mGeofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent())
+                    .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            // Geofences added
+                            Toast.makeText(camera, "Geofences agregados!", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnFailureListener(this, new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Failed to add geofences
+                            Toast.makeText(camera, "Error al agregar Geofences!", Toast.LENGTH_LONG).show();
+                        }
+                    });
+        } catch (SecurityException e){
+            Toast.makeText(camera, "Error por falta de permisos de ubicacion!", Toast.LENGTH_LONG).show();
+        }
+    }
+
 
     public void cambioDeCueva(int nuevaCueva){
         customWorldHelper.updateObjects(nuevaCueva);
@@ -127,18 +214,12 @@ public class SimpleCamera extends AppCompatActivity implements OnClickBeyondarOb
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case 1:
-                Boolean camera = this.checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
-                Boolean location = this.checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+                camera = this.checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+                location = this.checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
                 if (location && camera) {
                     //se crea bien
-                    //startActivity(new Intent(SimpleCamera.this, SimpleCamera.class));
-                    //finish();
                 }
                 break;
         }
     }
-
-
-
-
 }
